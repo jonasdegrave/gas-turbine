04ca7116df4edcc428c7201b438921a33b2dde1f
Matheus Monteverde <mmmontev98@gmail.com>
2022-04-28 08:39:58 -0300

    polyval correction + engines modules

--- model_reference/components.py
+++ model_reference/components.py
@@ -158,15 +158,11 @@ class Compressor(tp.StaticThermalProcess):
 
     def set_n2(self, n2):
         prc_constants = np.array([-6.073, 1.4821E1, -1.0042E1, 2.2915])
-        #n2_pow = _generate_exp(len(prc_constants), n2)
-        np.polyval(prc_constants, n2)
-        #self.prc = self._prc0 * prc_constants @ n2_pow
+        self.prc = np.polyval(prc_constants, n2) * self._prc0
 
         n_c_constants = [ -1.1234, 2.1097, 1.8617E-2]
-        self.n_c = np.polyval(n_c_constants, n2)
+        self.n_c = np.polyval(n_c_constants, n2) * self._n_c0
 
-        # n2_pow = _generate_exp(len(n_c_constants), n2)
-        # self.n_c = self._n_c0 * n_c_constants @ n2_pow
 
     def sumarise(self):
         index = ['t02', 'p02', 't03', 'p03', 'gamma_c', 'n_c', 'prc']
---
+++ model_reference/turbofan.py
@@ -0,0 +1,64 @@
+import components as comp
+import pandas as pd
+
+class TurboJet:
+    def __init__(self, data:dict):
+        self._n2 = 1
+
+        self.air_entrance = comp.Diffuser_Adiab(
+            data.get('ta'), data.get('pa'),
+            data.get('gamma_d'), data.get('r'),
+            data.get('u_in'), data.get('n_d')
+        )
+
+        self.compressor = comp.Compressor(
+            self.air_entrance.t0f, self.air_entrance.p0f,
+            data.get('gamma_c'), data.get('r'),
+            data.get('n_c'), data.get('prc')
+        )
+
+        self.combustion_chamber = comp.CombustionChamber(self.compressor.t0f, self.compressor.p0f,
+            data.get('gamma_b'), data.get('r'),
+            data.get('t04'), data.get('cp_fuel'), data.get('pc_fuel'), data.get('n_b')
+        )
+
+        self.turbine = comp.Turbine(self.combustion_chamber.t0f, self.combustion_chamber.p0f,
+            data.get('gamma_t'), data.get('r'),
+            data.get('n_t'), self.compressor
+        )
+
+        self.nozzle = comp.Nozzle_Adiab(self.turbine.t0f, self.turbine.p0f,
+            data.get('gamma_n'), data.get('r'),
+            data.get('pa'), data.get('n_n')
+        )
+        self.components = [self.air_entrance, self.compressor, self.combustion_chamber, self.turbine, self.nozzle]
+
+    def set_n2(self, n2):
+        self.compressor.set_n2(n2)
+        self.combustion_chamber.set_n2(n2)
+        self.turbine.set_n2(n2)
+
+        self._n2 = n2
+
+        self.update_model()
+    
+    def update_model(self):
+        self.compressor.t0i, self.compressor.p0i = self.air_entrance.t0f, self.air_entrance.p0f
+
+        self.combustion_chamber.t0i, self.combustion_chamber.p0i = self.compressor.t0f, self.compressor.p0f
+
+        self.turbine.t0i, self.turbine.p0i = self.combustion_chamber.t0f, self.combustion_chamber.p0f
+
+        self.nozzle.t0i, self.nozzle.p0i = self.turbine.t0f, self.turbine.p0f
+
+    def sumarise(self):
+        data = pd.Series(dtype='float64')
+
+        for i in self.components:
+            comp_dict = i.sumarise()
+            for k,v in comp_dict.items():
+                data.loc[k] = v
+
+        return data.sort_index().to_frame(name=self._n2)
+
+
